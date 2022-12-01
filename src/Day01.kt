941256fdcee4df030513f62f97eceb6dd149c2ac
m.slocombe <matt.slocombe@polestar-eam.com>
2022-12-01 13:09:34 +0000

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,44 @@
+import kotlin.math.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var highestElf = 0
+        var thisElf = 0
+        input.forEachIndexed { index, value ->
+            val calories = value.toIntOrNull()
+            if (calories != null) {
+                thisElf += calories
+                if (index == input.lastIndex) {
+                    highestElf = max(highestElf, thisElf)
+                }
+            } else {
+                highestElf = max(highestElf, thisElf)
+                thisElf = 0
+            }
+        }
+        return highestElf
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elfList = mutableListOf<Int>()
+        var thisElf = 0
+        input.forEachIndexed { index, value ->
+            value.toIntOrNull()?.let {
+                thisElf += it
+                if (index == input.lastIndex) {
+                    elfList.add(thisElf)
+                }
+            } ?: run {
+                elfList.add(thisElf)
+                thisElf = 0
+            }
+        }
+        return elfList.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
