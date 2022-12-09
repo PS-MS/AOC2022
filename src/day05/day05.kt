5fd9060618e70671adf94e672a46716876f21682
m.slocombe <matt.slocombe@polestar-eam.com>
2022-12-09 12:01:53 +0000

    day05 part 2 solved (but horrible)

--- src/day05/day05.kt
+++ src/day05/day05.kt
@@ -31,24 +31,50 @@ fun main() {
          return String(columnLists.map { it.last()[1] }.toCharArray())
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): String {
+        val (boxes, commandLines) = input.split("\r\n\r\n")
+        val columns = boxes.lines().map { it.chunked(4) }.map { it.map { box -> box.trim() } }
+        val columnLists: MutableList<MutableList<String>> = mutableListOf()
+        columns.last().forEach { _ ->
+            columnLists.add(mutableListOf())
+        }
+        columns.reversed().forEachIndexed { rowIndex, row ->
+            if(rowIndex != 0) {
+                row.forEachIndexed { index, box ->
+                    if (box.isNotBlank()) {
+                        columnLists[index].add(box)
+                    }
+                }
+            }
+        }
+        commandLines.lines().forEach {
+            val (x, y, z) = it.split(" ").mapNotNull { value -> value.toIntOrNull() }
+            //move x from y to z
+            val boxes = with(columnLists[y - 1]) {
+                val pickup = takeLast(x)
+                repeat(x) { removeLast() }
+                pickup
+            }
+            columnLists[z - 1].addAll(boxes)
+        }
+
+        return String(columnLists.map { it.last()[1]}.toCharArray())
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = File("src", "day05/test.txt").readText()
     val p1Test = part1(testInput)
     println("p1 test = $p1Test")
-//    check(p1Test == 2)
-//
-//    val p2Test = part2(testInput)
-//    println("p2 test = $p2Test")
-//    check(part2(testInput) == 4)
-//
+    check(p1Test == "CMZ")
+
+    val p2Test = part2(testInput)
+    println("p2 test = $p2Test")
+    check(part2(testInput) == "MCD")
+
     val input = File("src", "day05/real.txt").readText()
     val p1Real = part1(input)
     println("p1 real = $p1Real")
-//
-//    val p2Real = part2(input)
-//    println("p2 real = $p2Real")
+
+    val p2Real = part2(input)
+    println("p2 real = $p2Real")
 }
--- src/template.kt
+++ src/template.kt
@@ -1,3 +1,4 @@
+import readInput
 
 fun main() {
     fun part1(input: List<String>): Int {
@@ -9,10 +10,19 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("day01/test")
-    check(part1(testInput) == 24000)
+    val testInput = readInput("dayXX/test")
+    val p1Test = part1(testInput)
+    println("p1 test = $p1Test")
+    check(p1Test == 2)
 
-    val input = readInput("day01/real")
-    println(part1(input))
-    println(part2(input))
+    val p2Test = part2(testInput)
+    println("p2 test = $p2Test")
+    check(part2(testInput) == 4)
+
+    val input = readInput("dayXX/real")
+    val p1Real = part1(input)
+    println("p1 real = $p1Real")
+
+    val p2Real = part2(input)
+    println("p2 real = $p2Real")
 }
